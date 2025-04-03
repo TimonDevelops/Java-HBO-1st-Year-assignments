a8c5870d582f42f8576ab8fb1212601af6275df0
Timon <t.kooiman89@gmail.com>
2025-04-03 20:10:52 +0200

    2b

--- college5/opdrachten/opdracht2/TVGids.java
+++ college5/opdrachten/opdracht2/TVGids.java
@@ -1,7 +1,21 @@
+import java.util.ArrayList;
+
 public class TVGids {
 
     public static void main(String[] args) {
-        TVProgramma tv1 = new Talkshow("Go", 8, 3, "Joep", "Coding");
-        System.out.println(tv1);
+        ArrayList<TVProgramma> TVgids = new ArrayList<>();
+ 
+        TVProgramma show1 = new Talkshow("Go", 8, 3, "Joep", "Coding");
+        TVgids.add(show1);
+        TVProgramma show2 = new Talkshow("Wonders", 20, 2, "Jens", "Nature");
+        TVgids.add(show2);
+        TVProgramma serie1 = new Serie("Casa", 40, 1, "Jackie", 1);
+        TVgids.add(serie1);
+        TVProgramma serie2 = new Serie("Dxtr", 30, 4, "John", 5);
+        TVgids.add(serie2);
+
+        for (TVProgramma tvProgramma : TVgids) {
+           System.out.println(tvProgramma);
+        }
     }
 }
