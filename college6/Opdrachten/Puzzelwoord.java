6ca965e77bfac3ecc3b906f428bbe854aa3367d3
Timon <t.kooiman89@gmail.com>
2025-04-19 13:03:02 +0200

    2a

---
+++ college6/Opdrachten/Puzzelwoord.java
@@ -0,0 +1,15 @@
+public class Puzzelwoord {
+String woord;
+
+    public Puzzelwoord(String woord) {
+        this.woord = woord;
+    }
+
+    public String getWoord() {
+        return woord;
+    }
+
+    public String toString() {
+        return "Puzzelwoord: " + woord;
+    }
+}
