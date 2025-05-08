05d95bb0826920a9f24066439a38cdc77865df34
Timon <t.kooiman89@gmail.com>
2025-05-08 17:28:11 +0200

    1f

--- college7/Opdrachten/Opdracht1/Writer.java
+++ college7/Opdrachten/Opdracht1/Writer.java
@@ -6,11 +6,17 @@ public class Writer {
     public static void main(String[] args) {
         PrintWriter writer = null;
         try {
-            writer = new PrintWriter("bestandsnaam.txt");
+            writer = new PrintWriter("bestand.txt");
             writer.println("hoofdstuk 1");
+
+            String[] namen = new String[]{"Michiel", "Amy", "Joost"};
+            for (int i = 0; i < 4; i++) {
+                writer.println(namen[i]);
+            }
+        } catch (FileNotFoundException ex) {
+            System.out.println("Bestand kan niet gemaakt worden: " + ex.getMessage());
+        } finally {
             writer.close();
-        } catch (FileNotFoundException e) {
-            System.out.println(e);
         }
     }
 }
