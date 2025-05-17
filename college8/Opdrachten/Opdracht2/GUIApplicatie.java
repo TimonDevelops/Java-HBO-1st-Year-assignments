c24449d66deedbddf8469cc9ac694fb8f2546f77
Timon <t.kooiman89@gmail.com>
2025-05-17 13:43:20 +0200

    2 final

--- college8/Opdrachten/Opdracht2/GUIApplicatie.java
+++ college8/Opdrachten/Opdracht2/GUIApplicatie.java
@@ -2,6 +2,7 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -17,36 +18,31 @@ public class GUIApplicatie extends JFrame {
         JLabel Oost = new JLabel("Oost");
         Oost.setOpaque(true);
         Oost.setBackground(Color.RED);
-        Oost.setBounds(10, 10, 80 ,80);
+
         JLabel West = new JLabel("West");
         West.setOpaque(true);
         West.setBackground(Color.CYAN);
-        West.setBounds(100, 10, 80 ,80);
+
         JLabel Noord = new JLabel("Noord");
         Noord.setOpaque(true);
         Noord.setBackground(Color.GREEN);
-        Noord.setBounds(200, 10, 80 ,80);
+
         JLabel Zuid = new JLabel("Zuid");
         Zuid.setOpaque(true);
         Zuid.setBackground(Color.YELLOW);
-        Zuid.setBounds(150, 300, 80 ,80);
+
         JLabel Centrum = new JLabel("Centrum");
         Centrum.setOpaque(true);
         Centrum.setBackground(Color.ORANGE);
-        Centrum.setBounds(160, 160, 80 ,80);
 
-        setLayout(new BorderLayout(0 ,0));
-
-        // visual changes
-        Dimension d = new Dimension(400, 100);
-        Zuid.setPreferredSize(d);
+        setLayout(new GridLayout(2, 3, 5 ,10));
     
         // addings
-        add(Oost, BorderLayout.EAST);
-        add(West, BorderLayout.WEST);
+        add(Oost);
+        add(West);
         add(Centrum);
-        add(Noord, BorderLayout.NORTH);
-        add(Zuid, BorderLayout.SOUTH);
+        add(Noord);
+        add(Zuid);
         
 
         setVisible(true);
